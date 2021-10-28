60dd4f1c3f5b4caad72fc3cd18f94cc08d5df229
Umair-Manzoor-47 <2020cs24@student.uet.edu.pk>
2021-10-29 03:59:37 +0500

    Ye ly C# ki files

---
+++ C#/gameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Gravity
+{
+    //class for every character
+    class gameObject
+    {
+        // pictureBoxes as character
+        PictureBox character;
+        public gameObject() // null constructor
+        {
+            
+        }
+        
+        public gameObject(PictureBox character) // constructor overloading
+        {
+            this.character = character;
+        }
+        
+        // return pictureBox
+        public PictureBox getCharacter()
+        {
+            return this.character;
+        }
+
+    }
+}
